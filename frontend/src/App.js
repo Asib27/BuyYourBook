f07e376ea3d1381c15154dd626ec79b823ed220b
asib27 <asib.rahman1927@gmail.com>
2022-07-20 14:44:37 +0600

    add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Hello from './components/hello';
 import SignIn from './components/signin';
@@ -6,7 +7,11 @@ import SignUp from './components/signup';
 function App() {
   return (
     <div className="App">
-      <SignUp/>
+      <Routes>
+        <Route path='/' element={<Hello/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/signin.js
+++ frontend/src/components/signin.js
@@ -93,6 +93,8 @@ export default function SignIn() {
 
               <ErrorMessage name='pass'/>
 
+              <br/>
+
               <Field
                 as={FormControlLabel}
                 type="checkbox"
@@ -117,7 +119,7 @@ export default function SignIn() {
                     </Link>
                 </Grid>
                 <Grid item>
-                    <Link href="#" variant="body2">
+                    <Link href="/signup" variant="body2">
                         {"Don't have an account? Sign Up"}
                     </Link>
                 </Grid>
