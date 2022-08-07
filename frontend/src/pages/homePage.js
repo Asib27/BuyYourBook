a6d3649d40fa2da1a77404df412447e6d7398ce4
asib27 <asib.rahman1927@gmail.com>
2022-08-07 16:47:14 +0600

    move books prop to top level

--- frontend/src/pages/homePage.js
+++ frontend/src/pages/homePage.js
@@ -10,15 +10,21 @@ function a11yProps(index) {
 }
 
 export default function HomePage(props) {
+    const Book = {
+        image: "https://covers.zlibcdn2.com/covers299/books/11/c1/d2/11c1d24ddd14c46f714572faf7cebe6b.jpg",
+        name: "The art ",
+        author: "Knuth"
+    };
+
     return (
         <Box >
             <Carousel wrapAround={true}
             slidesToShow={7} cellSpacing={20}>
-                <BookCardMedium/>
-                <BookCardMedium/>
-                <BookCardMedium/>
-                <BookCardMedium/>
-                <BookCardMedium/>
+                <BookCardMedium book={Book}/>
+                <BookCardMedium book={Book}/>
+                <BookCardMedium book={Book}/>
+                <BookCardMedium book={Book}/>
+                <BookCardMedium book={Book}/>
             </Carousel>
         </Box>
     );
