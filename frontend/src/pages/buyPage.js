b8e8e23dd059e43c6d3038435911ae307787ef2d
asib27 <asib.rahman1927@gmail.com>
2022-08-10 04:33:40 +0600

    add address page to buy page

--- frontend/src/pages/buyPage.js
+++ frontend/src/pages/buyPage.js
@@ -1,7 +1,133 @@
-import { Box } from "@mui/material";
+import { Box, Button, Card, FormControlLabel, Grid } from "@mui/material";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { Checkbox, TextField } from "formik-mui";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useCart } from "react-use-cart";
 import BookCardSmall from "../components/book_card_small";
+import * as Yup from 'yup';
+
+let addressResponse = "";
+
+const AddressCard = (props)=>{
+    return (
+        <Card sx={{width: '50%'}}>
+            <Formik
+            initialValues={{email: '', name: '', streetAddress: '', district: '', country: '', phone: '', BackupPhoneNo: '', checked: false}}
+            validationSchema={Yup.object({
+              email: Yup.string().email('Invalid email address').required('Required'),
+              name: Yup.string().required('Required'),
+              streetAddress: Yup.string().required("Required"),
+              district: Yup.string().required("Required"),
+              country: Yup.string().required("Required"),
+              phone: Yup.string().required("Required"),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+                addressResponse = values;
+                console.log(addressResponse);
+                setSubmitting(false);
+            }}
+          >
+            {formic => (
+                <Form>
+                    <Field 
+                        component={TextField}
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus  
+                    />
+                    <ErrorMessage name='email'/>
+
+                    <Field
+                        component={TextField}
+                        margin="normal"
+                        fullWidth
+                        id="name"
+                        label="Full Name"
+                        name="name"
+                        autoComplete="cc-name"
+                    />
+                    <ErrorMessage name='name'/>
+
+                    <Field
+                        component={TextField}
+                        margin="normal"
+                        fullWidth
+                        id="streetAddress"
+                        label="Street Address"
+                        name="streetAddress"
+                        autoComplete="street-address"
+                    />
+                    <ErrorMessage name='streetAddress'/>
+
+                    <Field
+                        component={TextField}
+                        margin="normal"
+                        fullWidth
+                        id="district"
+                        label="District"
+                        name="district"
+                        autoComplete="address-level1"
+                    />
+                    <ErrorMessage name='district'/>
+
+                    <Field
+                        component={TextField}
+                        margin="normal"
+                        fullWidth
+                        id="country"
+                        label="Country"
+                        name="country"
+                        autoComplete="country-name"
+                    />
+                    <ErrorMessage name='country'/>
+
+                    <Field
+                        component={TextField}
+                        type="tel"
+                        margin="normal"
+                        fullWidth
+                        id="phone"
+                        label="Phone Number"
+                        name="phone"
+                        autoComplete="tel"
+                    />
+                    <ErrorMessage name='phone'/>
+
+                    <Field
+                        component={TextField}
+                        type="tel"
+                        margin="normal"
+                        fullWidth
+                        id="backuoPhone"
+                        label="Backup Phone Number"
+                        name="backupPhoneNo"
+                        autoComplete="tel"
+                    />
+                    <ErrorMessage name='backupPhoneNo'/>
+
+                    <Button
+                        type="submit"
+                        color={!formic.values.checked?"warning":"success"}
+                        fullWidth
+                        variant="contained"
+                        onClick={()=> formic.values.checked=true}
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        {!formic.values.checked?"Not Checked":"Checked"}
+                    </Button>
+
+                </Form>
+            )}
+          </Formik>
+        </Card>
+    )
+}
 
 export default function BuyPage(props){
     const { setItems, items } = useCart();
@@ -41,19 +167,13 @@ export default function BuyPage(props){
     
 
     return (
-        <Box>
+        <Box sx={{ m: 2}}>
             {
                 items.map((item)=>{
-                    return (<BookCardSmall itemId={item.id}/>);
+                    return (<BookCardSmall key={item.id} itemId={item.id}/>);
                 })
             }
+            <AddressCard/>
         </Box>
-        // <DataGrid
-        // rows={rows}
-        // columns={columns}
-        // pageSize={5}
-        // rowsPerPageOptions={[5]}
-        // checkboxSelection
-        // />
     )
 };
