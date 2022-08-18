42a744285447615e371db7dd11d0b2b5a9ee10e4
Tanveer2719 <ar137375@gmail.com>
2022-08-18 10:27:00 +0600

    stripe used

--- backend/src/main/java/com/asib27/authentication/Payment/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.asib27.authentication.Payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
