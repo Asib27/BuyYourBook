bac45d26e45ccebe7a66b714ef854e58c2489f34
asib27 <asib.rahman1927@gmail.com>
2022-08-24 22:31:13 +0600

    add submodule

--- backend/src/main/java/com/asib27/authentication/Locations/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.asib27.authentication.Locations;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-
-}
--- backend/src/main/java/com/asib27/authentication/Publisher/PublisherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.asib27.authentication.Publisher;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PublisherRepository extends JpaRepository<Publisher, Long> {
-}
