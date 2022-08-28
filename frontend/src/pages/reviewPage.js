485d63486c8e6f73be4fa3f1760e856c3e412751
asib27 <asib.rahman1927@gmail.com>
2022-08-28 15:28:00 +0600

    connect rating viewer to backend

--- frontend/src/pages/reviewPage.js
+++ frontend/src/pages/reviewPage.js
@@ -6,16 +6,16 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import bookService from "../services/book.service";
 
-const ReviewLeftPan = (props)=>{
-    const rating_avg = reviewService.getRatingAvg();
-    const no_of_review = reviewService.getNoOfReview();
+const ReviewLeftPan = ({ratingAvg, noReview})=>{
+    reviewService.getRatingAvg();
+    reviewService.getNoOfReview();
 
     return (
         <Box pt={5}>
             <Stack alignItems='center'>
-                <Typography variant='h2'>{rating_avg}</Typography>
-                <Rating name="user-rating" defaultValue={rating_avg} precision={0.1} readOnly />
-                <Typography>{no_of_review} reviews</Typography>
+                <Typography variant='h2'>{ratingAvg}</Typography>
+                <Rating name="user-rating" defaultValue={ratingAvg} precision={0.1} readOnly />
+                <Typography>{noReview} reviews</Typography>
             </Stack>
         </Box>
     );
@@ -24,6 +24,8 @@ const ReviewLeftPan = (props)=>{
 const RatingViewer = (props)=>{
     const rating = props.rating_percent;
     const rating_text = props.rating_text;
+
+    console.log(rating, rating_text);
 
     return (
         <Stack direction='row' alignItems='center' spacing={2}>
@@ -36,11 +38,17 @@ const RatingViewer = (props)=>{
 }
 
 const ReviewRightPan = (props)=>{
-    const percents = reviewService.getPercents();
-    let elements = percents.map((val, idx)=>{
-        return (<RatingViewer key={idx} rating_percent={val} rating_text={idx+1}/>)
-    }).reverse();
+    const percents = props.percentReview;
+    const percentMap = {};
+    for(let p in percents){
+        percentMap[Math.floor(p)] = +percents[p];
+    }
+    let elements = [];
 
+    for(let i = 1; i <= 5; i++){
+        const val = percentMap[i] ?? 0;
+        elements.push((<RatingViewer key={i} rating_percent={val} rating_text={i}/>))
+    }
 
     return (
         <Box pr={10} pl={10} pb={5} pt={5}>
@@ -53,19 +61,50 @@ const ReviewRightPan = (props)=>{
 
 export default function ReviewPage(props){
     const [Book, setBook] = useState(undefined);
+    const [ratingAvg, setRatingAvg] = useState(undefined);
+    const [noReview, setNoReview] = useState(undefined);
+    const [percentReview, setPercentReview] = useState(undefined);
     const params = useParams();
 
     useEffect(() => {
         const bookId = params.isbn;
         const fetchData = async ()=>{
             let data = await bookService.getBookByIsbn(bookId);
-            console.log(data);
             setBook(data);
         }
         fetchData();
         
-    }, [params.isbn])
+    }, [params.isbn]);
     
+    useEffect(() => {
+        const isbn = params.isbn;
+        const fetchData = async ()=>{
+            let data = await reviewService.getRatingAvg(isbn);
+            setRatingAvg(data);
+        }
+        fetchData();
+        
+    }, [params.isbn]);
+
+    useEffect(() => {
+        const isbn = params.isbn;
+        const fetchData = async ()=>{
+            let data = await reviewService.getNoOfReview(isbn);
+            setNoReview(data);
+        }
+        fetchData();
+        
+    }, [params.isbn]);
+
+    useEffect(() => {
+        const isbn = params.isbn;
+        const fetchData = async ()=>{
+            let data = await reviewService.getPercents(isbn);
+            setPercentReview(data);
+        }
+        fetchData();
+        
+    }, [params.isbn]);
 
     return (
         <Stack sx={{width: '80%'}} spacing={5}>
@@ -77,13 +116,29 @@ export default function ReviewPage(props){
                 )
             }
             <Paper elevation={3} style={{margin: '20', padding: '50'}}>
-                <Typography alignSelf='flex-start' variant='h5'>Ratings and Reviews</Typography>
+                <Typography  variant='h5' sx={{pt: 3, pl: 5}}>Ratings and Reviews</Typography>
                 <Stack direction="row" spacing={2}>
                     <Box sx={{ width: '20%' }}>
-                        <ReviewLeftPan/>
+                        {
+                            (ratingAvg !== undefined && noReview !== undefined) ? (
+                                <ReviewLeftPan 
+                                    ratingAvg={ratingAvg}
+                                    noReview={noReview}
+                                />
+                            ):(
+                                <Skeleton sx={{m : 2}} variant="rectangular" height={200}/>
+                            )
+                        }
                     </Box>
                     <Box sx={{ width: '80%' }}>
-                        <ReviewRightPan/>
+                        {
+                            percentReview? (
+                                <ReviewRightPan percentReview={percentReview}/>
+                            ):(
+                                <Skeleton sx={{m : 2}} variant="rectangular" height={200}/>
+                            )
+                        }
+                        
                     </Box>
                 </Stack>
             </Paper>
--- frontend/src/services/review.service.js
+++ frontend/src/services/review.service.js
@@ -1,17 +1,39 @@
+import axios from "axios";
+import kConst from "../const";
+import AuthService from "./auth.service";
+
+const API_URL = kConst.base_url;
+
 const rating_avg = 4.1;
 const no_of_review = 100;
 const percents = [6, 5, 10, 30, 50];
 
-const getRatingAvg = ()=>{
-    return rating_avg;
+const axiosGetUtil = async(url)=>{
+    const token = AuthService.getToken().jwtToken;
+    const config = {
+        headers: { Authorization: `Bearer ${token}` }
+    };
+
+    let data = await axios.get(
+        url, config
+    ).catch(err => console.log(err.response));
+
+    return data;
 }
 
-const getNoOfReview = ()=>{
-    return no_of_review;
+const getRatingAvg = async(isbn)=>{
+    let data = await axiosGetUtil(API_URL + '/api/book/' + isbn + '/rating/average_rating');
+    return data.data;
 }
 
-const getPercents = ()=>{
-    return percents;
+const getNoOfReview = async(isbn)=>{
+    let data = await axiosGetUtil(API_URL + '/api/book/' + isbn + '/rating/count');
+    return data.data;
+}
+
+const getPercents = async(isbn)=>{
+    let data = await axiosGetUtil(API_URL + '/api/book/' + isbn + '/rating/rating_percentage');
+    return data.data;
 }
 
 const reviewService = {
